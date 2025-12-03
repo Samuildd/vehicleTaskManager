3e638b0be2e63eeffd45287e9ef1cbf88a0bb7b1
Samuildd <Samuilyilma113@gmail.com>
2025-12-03 17:54:31 +1300

    Feedback from MT

--- vehicleTaskManager/Program.cs
+++ vehicleTaskManager/Program.cs
@@ -12,21 +12,22 @@ namespace FullProject
 
     class Program
     {
-        public static List<string> Requests()
+        enum Requests
         {
-            return new List<string>
-            {
-                "add", "delete", "update", "view", "exit"
-            };
+                add,
+                delete,
+                update,
+                view,
+                exit
         }
 
 
 
         static void Main(string[] args)
         {
-            List<string> vehicles = new List<string>();
+            var vehicles = new List<string>();
 
-            introMessage("Welcome To The Vehicle Task Manager");
+            IntroMessage();
 
             bool machineRunning = true;
 
@@ -43,10 +44,10 @@ namespace FullProject
             editVehicle(ref vehicles, request);
 
             Console.WriteLine();
-            Console.Write("Would you like to carry on? (yes/no): ");
+            Console.Write("Are you finished? (yes/no): ");
             string? carryOn = Console.ReadLine();
 
-            if(carryOn?.ToLower() == "no")
+            if(carryOn?.ToLower() == "yes")
                 {
                     Console.WriteLine("Goodybye!....");
                     machineRunning = false;
@@ -56,9 +57,9 @@ namespace FullProject
 
 
         /// This method controls the introduction message for users.
-        static void introMessage(string message)
+        static void IntroMessage()
         {
-            Console.WriteLine(message);
+            Console.WriteLine("Welcome To The Vehicle Task Manager");
             Console.WriteLine();
         }
 
@@ -66,39 +67,23 @@ namespace FullProject
         /// This method controls the users question and runs a validity check before allowing changes into the task manager.
         static string userQuestion()
         {
-            Console.Write("Would you like to View, Add, Update, Delete a vehicle or Exit: ");
-            string? result = Console.ReadLine();
-            result = result.ToLower();
-            if(string.IsNullOrEmpty(result))
-            {
-                bool userQuestionCheckerEmpNull = false;
-                while(userQuestionCheckerEmpNull != true)
-                {  
+            while(true){
+
+            
+                Console.Write("Would you like to View, Add, Update, Delete a vehicle or Exit: ");
+                string? result = Console.ReadLine();
+                result = result.ToLower();
+                if(string.IsNullOrEmpty(result)){
                     Console.WriteLine("Your Answer CANNOT be EMPTY or NULL!");
-                    Console.Write("Would you like to View, Add, Update, Delete a vehicle or Exit: ");
-                    result = Console.ReadLine();
-                    if (Requests().Contains(result))
-                    {
-                        userQuestionCheckerEmpNull = true;
-                    }
+                    continue;
                 }
-            }
-            else if(!Requests().Contains(result)){
-                bool userQuestionCheckerContains = false;
 
-                while (userQuestionCheckerContains != true)
-                {
+                if(!Enum.IsDefined(typeof(Requests), result)  ){
                     Console.WriteLine("Please Enter A Correct Request('Add', 'Update', 'View', 'Delete', 'Exit')");
-                    Console.Write("Would you like to View, Add, Update or Delete a vehicle?: "); 
-                    result = Console.ReadLine();
-                    if (Requests().Contains(result))
-                    {
-                        userQuestionCheckerContains = true;
-                    }
+                    continue;
                 }
-
-            }
-            return result;
+                return result;
+            }   
         }
 
         /// This method is the main method that allows user control over the vehicle task manager.
@@ -147,7 +132,7 @@ namespace FullProject
             if (vehicles.Count == 0)
                 {
                     Console.WriteLine("No existing vehicles. Please add one first.");
-                    editVehicle(ref vehicles, "add");
+                    addRequest(ref vehicles, "add");
                     return;
                 }
 
@@ -183,16 +168,17 @@ namespace FullProject
         /// This method represents the delete request the vehicle task manager
         static void deleteRequest(ref List<string> vehicles, string request)
         {
-            Console.Write("Enter vehicle to delete: ");
-            string vehicleRemove = Console.ReadLine() ?? "";
-
             if (vehicles.Count == 0)
                 {
                     Console.WriteLine("No existing vehicles. Please add one first.");
-                    editVehicle(ref vehicles, "add");
+                    addRequest(ref vehicles, "add");
                     return;
                 }
+            
+            Console.Write("Enter vehicle to delete: ");
+            string vehicleRemove = Console.ReadLine() ?? "";
             bool remove = vehicles.Remove(vehicleRemove);
+
             if(remove)
                 {
                     Console.WriteLine($"{vehicleRemove} is removed");
